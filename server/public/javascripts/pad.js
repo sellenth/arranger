f5b227aa799fed68bba086fa160d5b0b8e65a4db
Halston <sellenth@oregonstate.edu>
2020-02-09 13:17:00 -0800

    starting the day 2-9

--- server/public/javascripts/pad.js
+++ server/public/javascripts/pad.js
@@ -1,42 +1,41 @@
-var canvas;
-var signaturePad;
-import {emission, socket} from './sock-share.js'
+import {emission} from './sock-share.js'
 
 window.addEventListener('DOMContentLoaded', init, false);
 
-var drawer = document.getElementById('drawer');
-var plotter = document.getElementById('plotter');
-var save_btn = document.getElementById('save_btn');
-var back_btn = document.getElementById('back_btn');
-save_btn.addEventListener('click', save);
-back_btn.addEventListener('click', show);
-var clear_btn = document.getElementById('clear_btn')
+let tophalf    = document.getElementById('top-half');
+let bothalf   = document.getElementById('bottom-half');
+let save_btn  = document.getElementById('save_btn');
+let back_btn  = document.getElementById('back_btn');
+let clear_btn = document.getElementById('clear_btn')
+let canvas    = document.getElementById('signature-pad');
+
+var signaturePad;
+
 
 function init() {
-  plotter.style.display = "none";
-  canvas = document.getElementById('signature-pad');
   signaturePad = new SignaturePad(canvas, {
     minWidth: 5,
     maxWidth: 10,
   });
+
+  save_btn.addEventListener('click', save);
+  back_btn.addEventListener('click', show);
+
   clear_btn.addEventListener('click', function() {
     signaturePad.clear();
   });
+
+  bothalf.style.display = "none";
 }
 
 function show(){
-    drawer.style.display = "block";
-    save_btn.style.display = "inline-block";
-    clear_btn.style.display = "inline-block";
-    plotter.style.display = "none";
+    tophalf.style.display = "block";
+    bothalf.style.display = "none";
 }
 
 function save(){
-  drawer.style.display = "none";
-  save_btn.style.display = "none";
-  clear_btn.style.display = "none";
-  plotter.style.display = "block";
-
+  tophalf.style.display = "none";
+  bothalf.style.display = "block";
   const data = signaturePad.toData();
   emission(data);
 }
