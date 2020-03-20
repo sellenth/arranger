114e3caf85d5f3effc0135dbb252da247d7794d4
Halston <sellenth@oregonstate.edu>
2020-03-20 14:59:22 -0700

    done for now

--- server/public/javascripts/load.js
+++ server/public/javascripts/load.js
@@ -1,124 +1,78 @@
-import {emission, get_from_db} from './sock-share.js'
-import {set_coords, getFilteredCoords} from './point-plot.js'
+import {get_from_db} from './sock-share.js'
+import {redraw} from './point-plot.js'
 
 window.addEventListener('DOMContentLoaded', init, false);
 
-let screen1   = document.getElementById('signature');
 let screen2   = document.getElementById('point');
 let btns1     = document.getElementById('btns_screen1')
 let btns2     = document.getElementById('btns_screen2')
-let save_btn  = document.getElementById('save_btn');
+let get_info_btn  = document.getElementById('get_btn');
 let back_btn  = document.getElementById('back_btn');
 let toast     = document.getElementById('toast');
 let clear_btn = document.getElementById('clear_btn')
 let march_num_in = document.getElementById("m_num_in");
 var data = [];
-var signaturePad;
+var fetched_x;
+var fetched_y;
 
 function init() {
   get_from_db();
+  get_info_btn.addEventListener('click', save);
 
-  /*
-  signaturePad = new SignaturePad(screen1, {
-    minWidth: 5,
-    maxWidth: 10,
-  });
-
-  save_btn.addEventListener('click', save);
-  back_btn.addEventListener('click', show);
-  $('#send_btn').on('click', send);
-  clear_btn.addEventListener('click', function() {
-    signaturePad.clear();
-  });
-
+  $('#btns_screen2').toggle(false);
   resizeCanvas();
-  */
 
   $('#toast').toast({delay: 3000})
   $('#toast-success').toast({delay: 3000})
   $('#spinner').toggle(false);
 }
 
+function parse_points(input){
+  var arr = [];
+  for (var i = 0; i < input.length; i+=2){
+    arr.push({x: input[i], y: input[i+1], time: 1, color: 'black'})
+  }
+  return arr;
+}
+
+function indicate(i, points){
+  redraw(10, points);
+  let ctx = screen2.getContext('2d');
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(points[i-1]['x'], points[i-1]['y'], 10, 10)
+  ctx.fillStyle = "#000000";
+}
+
 function update_view(db_dat){
-  // db_dat[0] is width of record
-  // db_dat[1] is height of record
-  // db_dat[the rest] is x then y coordinates of points
-  // just need to draw these points onto the pad
-  // then link the text box to tell field location of particular point
-  // then done ;)
+  fetched_x = db_dat[0];
+  fetched_y = db_dat[1];
+  data = parse_points(db_dat.slice(2));
+  redraw(10, data);
 }
 
 export {update_view};
 
 function resizeCanvas() {
   var ratio =  Math.max(window.devicePixelRatio || 1, 1);
-  screen1.width = screen1.offsetWidth * ratio;
-  screen1.height = screen1.offsetHeight * ratio;
-  screen1.getContext("2d").scale(ratio, ratio);
-  signaturePad.clear(); // otherwise isEmpty() might return incorrect value
-  screen2.width = screen1.width;
-  screen2.height = screen1.height;
-  signaturePad.fromData(data);
+  screen2.width = screen2.offsetWidth * ratio;
+  screen2.height = screen2.offsetHeight * ratio;
+  screen2.getContext("2d").scale(ratio, ratio);
 }
 
 window.addEventListener("resize", resizeCanvas);
 
-function show(){
-    screen1.style.display = "block";
-    btns1.style.display = "block";
-    screen2.style.display = "none";
-    btns2.style.display = "none";
-    resizeCanvas();
-    signaturePad.fromData(data);
-}
-
 function save(){
-  if (isNaN(march_num_in.value) || march_num_in.value == ''){
-    $('.toast-body').html("Please enter the number of marchers!");
+  if (isNaN(march_num_in.value) || march_num_in.value == '' ||
+      march_num_in.value < 1 || march_num_in.value > data.length){
+    $('.toast-body').html("Please enter a valid position number!");
     $('#toast').toast('show');
     march_num_in.focus();
   }
   else {
-    screen1.style.display = "none";
     btns1.style.display = "none";
     btns2.style.display = "block";
-    screen2.style.display = "block";
-    data = signaturePad.toData();
-    set_coords(data);
-  }
-}
-
-function minify_dat(d, arr){
-  for (var i = 0; i < d.length; i++){
-    let x = Math.floor(d[i].x)
-    let y = Math.floor(d[i].y)
-    arr.push(x);
-    arr.push(y);
-    //d[i] = {x: x, y: y}
-  }
-  //return d
-  return arr
-}
-
-function send(){
-  let dat = getFilteredCoords();
-  if( dat.length == 0) {
-    $('.toast-body').html("Try again, can't send an empty formation!");
-    $('#toast').toast('show');
-  }
-  else {
-    $('#spinner').toggle();
-    /*
-    let disp_size = {
-      width: screen2.clientWidth,
-      height: screen2.clientHeight
-    }
-    dat = minify_dat(dat);
-    dat.push(disp_size);
-    */
-
-    var arr = [screen2.clientWidth, screen2.clientHeight];
-    arr = minify_dat(dat, arr);
-    emission(arr);
+    indicate(march_num_in.value, data);
+    $('#x_label').html(Math.floor(data[march_num_in.value]['x'] / fetched_x * 100))
+    $('#y_label').html(Math.floor(data[march_num_in.value]['y'] / fetched_y * 53))
   }
 }
